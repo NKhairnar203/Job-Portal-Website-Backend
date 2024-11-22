812b972dace67b9c158bf6a3110a1ce44e5295ed
Nilesh Khairnar <nkhairnar543@gmail.com>
2024-11-22 19:07:49 +0530

    MongoDB

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-          "mongodb+srv://nkhairnar543:owpq9KHjzs2Khnl3@cluster0.n28rl.mongodb.net/job-portal?retryWrites=true&w=majority&appName=Cluster0"
-        );
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongodb connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
 export default connectDB;
