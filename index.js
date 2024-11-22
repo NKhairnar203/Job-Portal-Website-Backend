cf94ad927bc5b8eaefbda3adc0385ed828723cb1
Nilesh Khairnar <nkhairnar543@gmail.com>
2024-11-22 19:54:54 +0530

    add frontend Link

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: "https://job-portal-nk.netlify.app",
   credentials: true,
 };
 
